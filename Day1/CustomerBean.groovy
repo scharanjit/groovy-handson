8d8c7c5003065ed7586283d553062628a48168bd
scharanjit <charanjit.singh@imaginea.com>
2016-09-01 12:12:57 +0530

    Organizing Files

---
+++ Day1/CustomerBean.groovy
@@ -0,0 +1,11 @@
+class Customer {
+    // properties
+    Integer id
+    String name
+    Date dob
+    // sample code
+    static void main(args) {
+        def customer = new Customer(id:1, name:"Gromit", dob:new Date())
+        println("Hello ${customer.name}")
+    }
+}
