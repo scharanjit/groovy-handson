ecf8b5c1698b5208cd48095b765a35aeb1708775
scharanjit <charanjit.singh@imaginea.com>
2016-09-01 12:04:54 +0530

    Add project structure

---
+++ Day1.groovy
@@ -0,0 +1,8 @@
+//Steps for Groovu Console
+//1.) Downlaod Groovy binary distribution from here http://groovy-lang.org/download.html
+//2.) Extract from zip & place it in some where in ur machine
+//3.) Open Console & cd to groovy folder  and run this command ./groovy-2.4.7/bin/groovyConsole
+
+
+//println "hello World"
+println "hi world"
---
+++ README.md
@@ -0,0 +1,2 @@
+#Groovy Hands On
+ This project is for hands on practise on some of the concepts of groovy language
